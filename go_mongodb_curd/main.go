c0bf04019ec5cef007135a4c22673be030be3c04
tharun13055 <megatharun13055>
2025-02-14 18:24:50 +0530

    added the db:

--- go_mongodb_curd/main.go
+++ go_mongodb_curd/main.go
@@ -1,12 +1,30 @@
 package main
 
-import(
-  _ "net/http"
-  _ "gopkg.in/mgo.v2"
-  _ "github.com/julienschmidt/httprouter"
+import (
+	"net/http"
+	_ "net/http"
+
+	"github.com/julienschmidt/httprouter"
+	_ "github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
 )
 
-func main(){
+func main() {
+	r := httprouter.New()
 
+	uc := controllers.NewUserController(getSession())
+
+	r.GET("/user/:id", uc.GetUser)
+  r.POST("/user", uc.CreateUser)
+  r.DELETE("/user/:id", uc.DeleteUser)
+  http.ListenAndServe(":8080",r)
 }
 
+func getSession() *mgo.Session {
+	session, err := mgo.Dial("mongodb+srv://tharun:password@go.vpv6p.mongodb.net/?retryWrites=true&w=majority&appName=go")
+	if err != nil {
+		panic(err)
+	}
+	return session
+
+}
